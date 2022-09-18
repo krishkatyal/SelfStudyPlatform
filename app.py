50f09580593106e2ef9c1269db82d908dbd44418
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2022-09-18 11:38:20 +0530

    Read pdf data

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import os
 from flask import Flask,request
 from flask_cors import CORS
+import PyPDF2
 
 app = Flask(__name__)
 CORS(app)
 
+file = open('test.pdf', 'rb')
+reader=PyPDF2.PdfFileReader(file)
+
+text_in=""
+for i in range(reader.numPages):
+    page=reader.getPage(i)
+    page_text=page.extractText()
+    text_in+=page_text
+
 @app.route('/init')
 def hello():
-    return 'hello world'
+    return text_in
